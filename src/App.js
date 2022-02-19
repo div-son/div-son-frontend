8f7e9afbca44751a09fcd1c76baad522b9359f4d
ZipDemon <zipdemon123@gmail.com>
2022-02-19 18:53:52 +0100

    contact me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Form from './oauth/components/register/Form';
+// import Dashboard from './oauth/components/dashboard/Dashboard';
 
 export const App = () => {
   return (
     <div><Form/></div>
+    // <div><Dashboard/></div>
+
       )
 }
 
