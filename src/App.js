341bcd0b1068630b80d30cd3da6803f36dd33186
ZipDemon <zipdemon123@gmail.com>
2022-02-23 19:17:02 +0100

    validating user input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import LandingPage from "./components/dashboard/LandingPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RegisterForm from "./components/register/RegisterForm";
+import LoginForm from "./components/login/LoginForm";
 
 export const App = () => {
   return (
-   
-      <div>
-        <LandingPage />
-      </div>
-    
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </Router>
+
+    </div>
   );
 };
 
--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import {
+  Grid,
+  Paper,
+  Avatar,
+  TextField,
+  Button,
+  Typography,
+  Link,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import { FormControlLabel } from "@material-ui/core";
+import { Checkbox } from "@material-ui/core";
 
-export const LoginForm = () => {
+export const LoginForm = ({ handleChange }) => {
+  const paperStyle = {
+    padding: 20,
+    height: "73vh",
+    width: 300,
+    margin: "0 auto",
+  };
+
+  const avatarStyle = { backgroundColor: "#1bbd7e" };
+  const btnstyle = { margin: "8px 0" };
+
   return (
-    <div>LoginForm</div>
-  )
-}
+    <Grid>
+      <Paper style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <h2>Div-son sign In page</h2>
+        </Grid>
+
+        <TextField
+          label="Email"
+          placeholder="Enter email"
+          type="email"
+          fullWidth
+          required
+        />
+
+        <TextField
+          label="Password"
+          placeholder="Enter password"
+          type="password"
+          fullWidth
+          required
+        />
+
+        <FormControlLabel
+          control={<Checkbox name="checkedB" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+          style={btnstyle}
+          fullWidth
+        >
+          Log in
+        </Button>
+        <Typography>
+          <Link href="#">Forgot password ?</Link>
+        </Typography>
+        <Typography>
+          Don't have an account?
+          <Link to="/register" onClick={() => handleChange("event", 1)}>
+            Sign Up
+          </Link>
+        </Typography>
+      </Paper>
+    </Grid>
+  );
+};
 
 export default LoginForm;
